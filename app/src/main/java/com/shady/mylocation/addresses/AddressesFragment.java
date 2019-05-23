ff8c8e3c8ac8dd4a62f959aafb25d2388ef8d0d9
shady <shady@nweave.com>
2019-05-23 03:06:02 +0200

    - Implemented autocomplete address feature - Added autocomplete fargment in a separate - navigate from map fragment to addresses fragment

---
+++ app/src/main/java/com/shady/mylocation/addresses/AddressesFragment.java
@@ -0,0 +1,68 @@
+package com.shady.mylocation.addresses;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.shady.mylocation.R;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.Arrays;
+
+import static org.greenrobot.eventbus.EventBus.TAG;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddressesFragment extends Fragment {
+
+
+    public AddressesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_addresses, container, false);
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.setCountry("eg");
+        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
+                new LatLng(22.811671, 25.617695), new LatLng(30.954095, 34.055196)));
+// Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+// Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                EventBus.getDefault().post(place);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+        return view;
+    }
+
+}
